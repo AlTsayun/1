09b7a2ea65c0f4d435da832c13f6c9823182faa6
okko <al.tsayun@gmail.com>
2021-09-03 14:17:47 +0300

    implemented  UnifornDistributionMetrics

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from lemer import Lemer
 from sequence_metrics import SequenceMetrics
+from uniform_distribution_metrics import UniformDistributionMetrics
 
-lemer = Lemer(3,5,1)
+lemer = Lemer(3,5,3)
 
-sequence = [lemer.getNext() for i in range(0,5)]
+sequence = [lemer.getNext() for i in range(0,50)]
 print(sequence)
 metrics = SequenceMetrics(sequence)
 print(metrics.getExpectedValue())
 print(metrics.getVariance())
 print(metrics.getStandardDeviation())
+
+fromInclusive = 0
+toInclusive = 1
+metrics = UniformDistributionMetrics(fromInclusive, toInclusive)
+
+print(metrics.getExpectedValue())
+print(metrics.getVariance())
+print(metrics.getStandardDeviation())
+
+func = metrics.getCumulativeDistributionFunction()
+print("CumulativeDistributionFunction:")
+for i in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
+    print(func(i))
+
+func = metrics.getProbabilityDensityFunction()
+print("ProbabilityDensityFunction:")
+for i in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
+    print(func(i))
+
---
+++ uniform_distribution_metrics.py
@@ -0,0 +1,27 @@
+import math
+class UniformDistributionMetrics:
+    def __init__(self, fromInclusive, toInclusive):
+        self.fromInclusive = fromInclusive
+        self.toInclusive = toInclusive
+
+        #мат ожидание
+        self.expectedValue = float(fromInclusive + toInclusive) / 2 
+        #дисперсия
+        self.variance = float(toInclusive - fromInclusive) ** 2 / 12 
+        #среднее квадратичное отклонение
+        self.standardDeviation =  float(toInclusive - fromInclusive) / (2 * math.sqrt(3))
+
+    def getExpectedValue(self):
+        return self.expectedValue
+
+    def getVariance(self):
+        return self.variance
+
+    def getStandardDeviation(self):
+        return self.standardDeviation
+
+    def getProbabilityDensityFunction(self):
+        return lambda x: 0 if (x < self.fromInclusive) or (x > self.toInclusive) else float(1) / (self.toInclusive - self.fromInclusive) 
+
+    def getCumulativeDistributionFunction(self):
+        return lambda x: 0 if (x < self.fromInclusive) else 1 if (x > self.toInclusive) else  float(x - self.fromInclusive) / (self.toInclusive - self.fromInclusive) 
